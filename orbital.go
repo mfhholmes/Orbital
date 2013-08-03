09c35533f99807782a97a90cc88397eb98f37c55
Royce Townsend <github@roycetownsend.com>
2013-08-03 12:17:04 +0800

    Orbital http server: accepts requests does nothing useful with them

---
+++ orbital.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "github.com/gorilla/mux"
+  "encoding/json"
+)
+
+type Message struct {
+  CurrentPopulation int
+}
+
+func pop(response http.ResponseWriter, request *http.Request) {
+  status := Message{5}
+  json, _ := json.Marshal(status)
+  fmt.Fprintf(response, "%s", json)
+}
+
+func setPop(response http.ResponseWriter, request *http.Request) {
+  start := request.FormValue("startPopulation")
+  growth := request.FormValue("growthRate")
+  fmt.Fprintf(response, "%s + %s per ?", start, growth)
+}
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/population", pop).Methods("GET")
+  r.HandleFunc("/population", setPop).Methods("POST")
+  http.Handle("/", r)
+  http.ListenAndServe(":8080", nil)
+}
