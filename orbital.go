33232a28761c1222bd547807fbad30e7a0c1b64a
Royce Townsend <github@roycetownsend.com>
2013-08-03 13:04:04 +0800

    Pass the request for static files through the mux

--- orbital.go
+++ orbital.go
@@ -28,7 +28,7 @@ func main() {
   r := mux.NewRouter()
   r.HandleFunc("/population", pop).Methods("GET")
   r.HandleFunc("/population", setPop).Methods("POST")
-  http.Handle("/population", r)
-  http.Handle("/",http.FileServer(http.Dir("public_html")))
+  r.PathPrefix("/").Handler(http.FileServer(http.Dir("public_html")))
+  http.Handle("/", r)
   http.ListenAndServe(":8080", nil)
 }
