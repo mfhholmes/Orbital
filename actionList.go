a1cfcadaf186e30a77e39a64951459a368eae587
Marcus Holmes <mfhholmes@gmail.com>
2013-08-04 16:45:33 +0800

    fixed web server - it didn't like being in a goroutine - need to investigate

--- actionList.go
+++ actionList.go
@@ -1,94 +1,100 @@
 package main
 
-import("sort")
-import("time")
-import("fmt")
-
+import (
+	"sort"
+)
+import (
+	"time"
+)
+import (
+	"fmt"
+)
 
 var isRunning bool
 
 type processFunc func()
 
-type controlInstruction struct{
-  runLoop bool
-  delay time.Duration
+type controlInstruction struct {
+	runLoop bool
+	delay   time.Duration
 }
 
-type actionItem struct{
-  execTime time.Time
-  process processFunc
+type actionItem struct {
+	execTime time.Time
+	process  processFunc
 }
 
 //ActionList is a list of Action Items, which is then
-type ActionList struct{
-  items []*actionItem
+type ActionList struct {
+	items []*actionItem
 }
-func (al *ActionList) Pop() *actionItem{
-  if (len(al.items) == 0){
-    // return an item that does nothing and is set to run an hour from now
-    return &actionItem{execTime:time.Now().Add(time.Hour),process:func(){}}
-  }
-  var result = al.items[0]
-  al.items = al.items[1:]
-  return result
+
+func (al *ActionList) Pop() *actionItem {
+	if len(al.items) == 0 {
+		// return an item that does nothing and is set to run an hour from now
+		return &actionItem{execTime: time.Now().Add(time.Hour), process: func() {}}
+	}
+	var result = al.items[0]
+	al.items = al.items[1:]
+	return result
 }
-func (al ActionList) Peek(index int) *actionItem{
-  if(index <0){
-    index =0
-  }
-  if (len(al.items) == 0){
-    // return an item that does nothing and is set to run a year from now
-    return &actionItem{execTime:time.Now().Add(time.Hour),process:func(){}}
-  }
-  if(index > len(al.items)){
-    index = len(al.items)-1
-  }
-  return al.items[index]
+func (al ActionList) Peek(index int) *actionItem {
+	if index < 0 {
+		index = 0
+	}
+	if len(al.items) == 0 {
+		// return an item that does nothing and is set to run a year from now
+		return &actionItem{execTime: time.Now().Add(time.Hour), process: func() {}}
+	}
+	if index > len(al.items) {
+		index = len(al.items) - 1
+	}
+	return al.items[index]
 }
-func (al *ActionList) Push(newItem *actionItem){
-  al.items = append(al.items, newItem)
-  sort.Sort(al)
+func (al *ActionList) Push(newItem *actionItem) {
+	al.items = append(al.items, newItem)
+	sort.Sort(al)
 }
 func (al ActionList) Len() int {
-  return len(al.items)
+	return len(al.items)
 }
 
 func (al ActionList) Less(i, j int) bool {
-  return al.items[i].execTime.Before( al.items[j].execTime)
+	return al.items[i].execTime.Before(al.items[j].execTime)
 }
-func (al ActionList) Swap(i,j int) {
-  al.items[i], al.items[j] = al.items[j], al.items[i]
+func (al ActionList) Swap(i, j int) {
+	al.items[i], al.items[j] = al.items[j], al.items[i]
 }
 func (al ActionList) listStatus() string {
-  if(len(al.items) > 0){
-    return fmt.Sprintf("items: %d earliest: %s",len(al.items),al.items[0].execTime.String())
-  }
-  return "no items in queue"
+	if len(al.items) > 0 {
+		return fmt.Sprintf("items: %d earliest: %s", len(al.items), al.items[0].execTime.String())
+	}
+	return "no items in queue"
 }
 
-func (al *ActionList) mainLoop(controlChannel chan controlInstruction){
-  timeout := make(chan bool, 1)
-  runLoop := true
-  delay := (100 * time.Millisecond)
-  for runLoop{
-    isRunning = true
-    if(al.Len() > 0){
-      for al.Peek(0).execTime.Before(time.Now()){
-        go al.Pop().process()
-      }
-    }
-    // kicks off a 100ms delay, which is used to check the control channel for control instructions
-    go func() {
-      time.Sleep(delay)
-      timeout <- true
-    }()
-    select {
-      case command := <-controlChannel:
-        runLoop = command.runLoop
-        delay = command.delay
-      case <-timeout:
-        // timeout, so continue around the loop
-    }
-  }
-  isRunning = false
+func (al *ActionList) mainLoop(controlChannel chan controlInstruction) {
+	timeout := make(chan bool, 1)
+	runLoop := true
+	delay := (100 * time.Millisecond)
+	for runLoop {
+		isRunning = true
+		if al.Len() > 0 {
+			for al.Peek(0).execTime.Before(time.Now()) {
+				go al.Pop().process()
+			}
+		}
+		// kicks off a 100ms delay, which is used to check the control channel for control instructions
+		go func() {
+			time.Sleep(delay)
+			timeout <- true
+		}()
+		select {
+		case command := <-controlChannel:
+			runLoop = command.runLoop
+			delay = command.delay
+		case <-timeout:
+			// timeout, so continue around the loop
+		}
+	}
+	isRunning = false
 }
--- orbital.go
+++ orbital.go
@@ -1,50 +1,49 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gorilla/mux"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
+
 //import("time")
 
-
 type Message struct {
-  CurrentPopulation int
+	CurrentPopulation int
 }
 
 func pop(response http.ResponseWriter, request *http.Request) {
-  status := Message{5}
-  json, _ := json.Marshal(status)
-  fmt.Fprintf(response, "%s", json)
+	status := Message{5}
+	json, _ := json.Marshal(status)
+	fmt.Fprintf(response, "%s", json)
 }
 
 func setPop(response http.ResponseWriter, request *http.Request) {
-  start := request.FormValue("startPopulation")
-  growth := request.FormValue("growthRate")
-  fmt.Fprintf(response, "%s + %s per ?", start, growth)
+	start := request.FormValue("startPopulation")
+	growth := request.FormValue("growthRate")
+	fmt.Fprintf(response, "%s + %s per ?", start, growth)
 }
 
-
-
-
+func buildHandler() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/population", pop).Methods("GET")
+	r.HandleFunc("/population", setPop).Methods("POST")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public_html")))
+	return r
+}
 
 func main() {
 
-  r := mux.NewRouter()
-  r.HandleFunc("/population", pop).Methods("GET")
-  r.HandleFunc("/population", setPop).Methods("POST")
-  r.PathPrefix("/").Handler(http.FileServer(http.Dir("public_html")))
-  http.Handle("/", r)
+	fmt.Println("firing up actionList")
+	actions := new(ActionList)
+	controlChan := make(chan controlInstruction)
+	go actions.mainLoop(controlChan)
 
-  fmt.Println("firing up webserver")
-  go http.ListenAndServe(":8080", nil)
+	fmt.Println("firing up webserver")
+	handler := buildHandler()
 
-  fmt.Println("firing up actionList")
-  actions := new(ActionList)
-  controlChan := make(chan controlInstruction)
-  actions.mainLoop(controlChan)
+	http.ListenAndServe(":1234", handler)
 
-  fmt.Println("server ended")
+	fmt.Println("server ended")
 }
-
